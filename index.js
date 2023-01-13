f7830dd2502d3e61e250d5f724297616048582d9
studmanj <bjackso3@hotmail.com>
2023-01-13 01:06:44 -0700

    Practice code

---
+++ index.js
@@ -0,0 +1,15 @@
+function A (arr){
+    const even = []
+    const odd = []
+        for(num of arr) {
+            if (arr[num] % 2 == 0) {
+                even.push(arr[num])
+            }
+            else {
+                odd.push(arr[num])
+            }
+        }
+    console.log(even)
+    console.log(odd)
+}
+A([1,2,3,4,5,4,3,2,2])
